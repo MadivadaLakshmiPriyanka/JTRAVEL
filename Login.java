392a76d7c07527d7ae5494bc9cfc61eb85106dba
MadivadaLakshmiPriyanka <pinky.mlp@gmail.com>
2017-06-26 00:35:21 +0530

    Add files via upload

---
+++ Login.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class Login {
+	public static void main(String args[])
+	{
+		Scanner input = new Scanner(System.in);
+
+	    String username;
+	    String password;
+
+	    System.out.println("Log In");
+	    System.out.println("Username: ");
+	    username = input.nextLine();
+
+	    System.out.println("Password: ");
+	    password = input.nextLine();
+
+	    
+
+
+	}
+
+}
