47e6c44b37b111384ef51179c19c46cd09cf2c6c
MadivadaLakshmiPriyanka <pinky.mlp@gmail.com>
2017-06-26 16:24:37 +0530

    Add files via upload

---
+++ Userpage.java
@@ -0,0 +1,16 @@
+
+public class Userpage {
+	public static void main(String args[])
+	{
+		String link = "<a href='MY RESERVATIONS' target='_blank'>MY RESERVATIONS</a>";
+		String link1 = "<a href='NEW RESERVATIONS' target='_blank'>NEW RESERVATIONS</a>";
+		String link2 = "<a href='RULES' target='_blank'>RULES</a>";
+		String link3 = "<a href='LATEST NEWS' target='_blank'>LATEST NEWS</a>";
+		System.out.println(link);
+		System.out.println(link1);
+		System.out.println(link2);
+		System.out.println(link3);
+		
+	}
+
+}
